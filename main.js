3b2f467d8785c3e7b51236d84a41478890141ded
SeawardSalmon6 <rayhanachour@hotmail.com>
2021-09-07 22:37:28 -0300

    updating FAQ section and footer + globe-svg

---
+++ main.js
@@ -0,0 +1,18 @@
+function questions(element) {
+    let faqAnswer = element.nextElementSibling;
+    let svgClose = element.firstElementChild;
+    
+    if(faqAnswer.classList.contains("closed")) {
+        faqAnswer.classList.remove("closed");
+        faqAnswer.classList.add("open");
+        
+        svgClose.classList.remove("closed");
+        svgClose.classList.add("open");
+    } else {    
+        faqAnswer.classList.remove("open");
+        faqAnswer.classList.add("closed");
+        
+        svgClose.classList.remove("open");
+        svgClose.classList.add("closed");
+    }
+}
