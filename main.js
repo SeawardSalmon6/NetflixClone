e36140943b5bdcba2699c0706cd115a793207625
SeawardSalmon6 <rayhanachour@hotmail.com>
2021-09-09 16:29:33 -0300

    upgrading JS and CSS elements

--- main.js
+++ main.js
@@ -1,20 +1,6 @@
-function questions(element) {
-    let faqAnswer = element.nextElementSibling;
-    let svgClose = element.firstElementChild;
-    
-    if(faqAnswer.classList.contains("closed")) {
-        faqAnswer.classList.remove("closed");
-        faqAnswer.classList.add("open");
-        
-        svgClose.classList.remove("closed");
-        svgClose.classList.add("open");
-    } else {    
-        faqAnswer.classList.remove("open");
-        faqAnswer.classList.add("closed");
-        
-        svgClose.classList.remove("open");
-        svgClose.classList.add("closed");
-    }
+function openQuestion(btn) {
+	btn.firstElementChild.classList.toggle("open");
+	btn.nextElementSibling.classList.toggle("open");
 }
 
 function playAudio() {
